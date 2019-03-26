9c2bc78d7b252de1c7cbec8ad21e08688fd2f1d0
Sixian Lin <davidlin006811@gmail.com>
2019-03-25 23:48:57 -0400

    add theme.json and theme router

---
+++ routers/themeRouter.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+router.get("/all", (req, res) => {
+  const rawdata = fs.readFileSync("theme.json");
+  const theme = JSON.parse(rawdata);
+  res.status(200).json(theme);
+});
+
+//post a new theme
+router.post("/", (req, res) => {
+  const data = req.body;
+  io.sockets.emit("new theme", req.body);
+  const rawdata = fs.readFileSync("theme.json");
+  const theme = JSON.parse(rawdata);
+  theme.presets.push(data);
+  fs.writeFileSync("theme.json", JSON.stringify(theme));
+  res.status(200).json({ code: 0, message: "save theme successfully" });
+});
+
+router.patch("/", (req, res) => {
+  io.sockets.emit("select theme", req.body.selectedPresetIndex);
+  const rawdata = fs.readFileSync("theme.json");
+  const theme = JSON.parse(rawdata);
+  theme.selectedPresetIndex = req.body.selectedPresetIndex;
+  fs.writeFileSync("theme.json", JSON.stringify(theme));
+  res.status(200).json({ code: 0, message: "select theme successfully" });
+});
+module.exports = router;
