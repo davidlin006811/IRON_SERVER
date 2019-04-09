e9780e6c2ac1329975dd1d4d0721cffb5184ed20
Sixian Lin <davidlin006811@gmail.com>
2019-04-09 14:08:17 -0400

    modify admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,7 +1,7 @@
 module.exports = {
   email: "lin00132@algonquinlive.com",
   phone: "6138623030",
-  SMSNumber: "6138623030@sms.rogers.com",
+  SMSNumber: "6138623030@pcs.rogers.com",
   tokenSecret: "IronRatsCustomOttawa",
   role: "admin"
 };
